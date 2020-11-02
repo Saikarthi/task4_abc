d8847d8af31f2f2c553fab8294408069252ea603
Saikarthi <41743913+Saikarthi@users.noreply.github.com>
2020-11-02 21:48:59 +0530

    done

---
+++ task4/Assets/force.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class force : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float f= 5f;
+    public Vector3 pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = new Vector3(0, 0, -1f);
+        rb = this.GetComponent<Rigidbody>();
+        rb.AddForce(pos * f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ task4/Assets/manger.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class manger : MonoBehaviour
+{
+    public GameObject panel;
+    public GameObject outpanel;
+    public GameObject correct;
+    public Text time;
+    public Text score;
+    private float point =6f;
+    float timeLeft= 0f;
+
+    void Start()
+    {
+       
+    }
+
+
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        time.text = (timeLeft).ToString("0");
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Sphere")
+        {
+            timeLeft = 60f;
+            panel.SetActive(true);
+            Invoke("off", 60f);
+        }
+    }
+    public void off()
+    {
+       
+        panel.SetActive(false);
+        outpanel.SetActive(true);
+
+
+    }
+
+    public void tiger()
+    {
+        if (timeLeft < 60 && timeLeft > 50)
+        {
+            point = 6f;
+        }
+        else if (timeLeft < 50 && timeLeft > 40)
+        {
+
+            point = 4f;
+
+        }
+        else if (timeLeft < 40 && timeLeft > 0)
+        {
+
+            point = 2f;
+
+        }
+        score.text = point.ToString(); 
+        panel.SetActive(false);
+        correct.SetActive(true);
+
+
+    }
+}
